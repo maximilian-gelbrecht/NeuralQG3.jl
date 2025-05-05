316bc747d29072d036f6d379c827cc84d6a344af
Max <maximilian.gelbrecht@posteo.de>
2025-05-05 16:19:16 +0200

    spectra computed

--- scripts/plot_spectrum.jl
+++ scripts/plot_spectrum.jl
@@ -5,11 +5,9 @@ using Plots, JLD2, StatsBase, Statistics
 
 BASE_NAME = "results/qg3-long/Spectrum-PSN-qg3-ev0-m36-"
 
-S1 = jldopen(string(BASE_NAME,"1.jld2"))
-
 function plot_angular_power(BASE_NAME, i_lvl, title=""; N=20)
 
-    fig = plot(S1[i_lvl], yscale=:log10, xlabel="Wavenumber l", ylabel="Average Angular Power", title=title)
+    fig = plot(yscale=:log10, xlabel="Wavenumber l", ylabel="Average Angular Power", title=title)
     for i=1:N
         S1 = jldopen(string(BASE_NAME,i,".jld2"))
 
@@ -36,17 +34,19 @@ plot(p1, p2, p3, p4, p5, p6, layout=(2,3), size=(800,600))
 
 savefig("spectra-qg3.pdf")
 
-BASE_NAME = "results/speedy/Spectrum-PSN-speedy-ev3-m36-"
+#BASE_NAME = "results/speedy/Spectrum-PSN-speedy-ev3-m36-"
+BASE_NAME = "results/speedy/Spectrum-4-PSN-speedy-ev4-m48-"
 
 p1 = plot_angular_power(BASE_NAME, "S_1", "PSN 200 hPa", N=10)
 p2 = plot_angular_power(BASE_NAME, "S_2", "PSN 500 hPa", N=10)
 p3 = plot_angular_power(BASE_NAME, "S_3", "PSN 850 hPa", N=10)
 
-BASE_NAME = "results/speedy/Spectrum-Baseline-speedy-ev3-m36-"
+#BASE_NAME = "results/speedy/Spectrum-Baseline-speedy-ev4-m36-"
+BASE_NAME = "results/speedy/Spectrum-4-Baseline-speedy-ev4-m48-"
 
-p4 = plot_angular_power(BASE_NAME, "S_1", "PS UNET 200 hPa", N=3)
-p5 = plot_angular_power(BASE_NAME, "S_2", "PS UNET 500 hPa", N=3)
-p6 = plot_angular_power(BASE_NAME, "S_3", "PS UNET 850 hPa", N=3)
+p4 = plot_angular_power(BASE_NAME, "S_1", "PS UNET 200 hPa", N=2)
+p5 = plot_angular_power(BASE_NAME, "S_2", "PS UNET 500 hPa", N=2)
+p6 = plot_angular_power(BASE_NAME, "S_3", "PS UNET 850 hPa", N=2)
 
 plot(p1, p2, p3, p4, p5, p6, layout=(2,3), size=(800,600))
 
